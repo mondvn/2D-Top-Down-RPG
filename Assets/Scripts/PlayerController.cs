3ecf0920881f66da3823df2d1fa6d98f94529869
Mond <trannamanh94@gmail.com>
2023-04-28 23:21:03 +0700

    10. Animating The Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         this.playerControls = new PlayerControls();
         this.rb = GetComponent<Rigidbody2D>();
+        this.myAnimator = GetComponent<Animator>();
+        this.mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -27,16 +31,35 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        this.AdjustPlayerFacingDirection();
         this.Move();
     }
 
     private void PlayerInput()
     {
         this.movement = this.playerControls.Movement.Move.ReadValue<Vector2>();
+        this.myAnimator.SetFloat("moveX", movement.x);
+        this.myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
     {
         this.rb.MovePosition(this.rb.position + this.movement * (this.moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
+
     }
 }
