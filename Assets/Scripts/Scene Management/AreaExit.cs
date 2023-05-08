d3a038ff59cdd822e91f36d58fcac09b22f43af1
Mond <trannamanh94@gmail.com>
2023-05-08 20:17:00 +0700

    36. Scene Entry

--- Assets/Scripts/Scene Management/AreaExit.cs
+++ Assets/Scripts/Scene Management/AreaExit.cs
@@ -6,11 +6,15 @@ using UnityEngine.SceneManagement;
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.gameObject.GetComponent<PlayerController>()) return;
-        SceneManager.LoadScene(sceneToLoad);
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            SceneManager.LoadScene(this.sceneToLoad);
+            SceneManagement.Instance.SetTransitionName(this.sceneTransitionName);
+        }
     }
 }
--- Assets/Scripts/Scene Management/Singleton.cs
+++ Assets/Scripts/Scene Management/Singleton.cs
@@ -9,8 +9,14 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 
     protected virtual void Awake()
     {
-        if (instance != null && this.gameObject != null) Destroy(this.gameObject);
-        instance = (T)this;
+        if (instance != null && gameObject != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = (T)this;
+        }
 
         DontDestroyOnLoad(gameObject);
     }
