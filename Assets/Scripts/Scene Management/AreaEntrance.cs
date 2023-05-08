7266bc19969b6d00037b8c92c3a4ca73f3aac970
Mond <trannamanh94@gmail.com>
2023-05-08 20:32:54 +0700

    37. Camera Controller

--- Assets/Scripts/Scene Management/AreaEntrance.cs
+++ Assets/Scripts/Scene Management/AreaEntrance.cs
@@ -11,6 +11,7 @@ public class AreaEntrance : MonoBehaviour
         if (this.transitionName == SceneManagement.Instance.SceneTransitionName)
         {
             PlayerController.Instance.transform.position = this.transform.position;
+            CameraController.Instance.SetPlayerCameraFollow();
         }
     }
 
