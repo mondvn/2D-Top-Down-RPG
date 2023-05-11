9ac6f59d6fe7354a6541f2fe348c0021b86ae540
Mond <trannamanh94@gmail.com>
2023-05-12 00:06:13 +0700

    62. Enemies Galore

--- Assets/Scripts/Scene Management/CameraController.cs
+++ Assets/Scripts/Scene Management/CameraController.cs
@@ -7,9 +7,14 @@ public class CameraController : Singleton<CameraController>
 {
     private CinemachineVirtualCamera cinemachineVirtualCamera;
 
+    private void Start()
+    {
+        this.SetPlayerCameraFollow();
+    }
+
     public void SetPlayerCameraFollow()
     {
-        cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
-        cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
+        this.cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        this.cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
     }
 }
