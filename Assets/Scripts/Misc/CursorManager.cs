d60a2b06c38926a5ddf7438634cac06c5920fb8b
Mond <trannamanh94@gmail.com>
2023-05-16 01:53:29 +0700

    81. Custom Cursor

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorManager : MonoBehaviour
+{
+    private Image image;
+
+    private void Awake()
+    {
+        this.image = GetComponent<Image>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        if (Application.isPlaying)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPos = Input.mousePosition;
+        this.image.rectTransform.position = cursorPos;
+
+        if (!Application.isPlaying) return;
+        Cursor.visible = false;
+    }
+}
